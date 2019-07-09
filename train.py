1769f3fa542d264924238b7fb3b97fb53e9dacc4
andreas <andreashi2@gmail.com>
2019-07-09 08:18:51 +0200

    epochs

--- train.py
+++ train.py
@@ -20,8 +20,8 @@ from tensorflow_datasets.core.features.text import TokenTextEncoder
 
 #dataset_x = dataset_x.map(lambda token: token.numpy().decode("utf-8"))
 
-dataset_x = tf.data.TextLineDataset("data/texts_noisy.txt").take(1000000)
-dataset_y = tf.data.TextLineDataset("data/texts.txt").take(1000000)
+dataset_x = tf.data.TextLineDataset("data/texts_noisy.txt").take(2000000)
+dataset_y = tf.data.TextLineDataset("data/texts.txt").take(2000000)
 dataset = Dataset.zip((dataset_x, dataset_y))
 
 
@@ -525,7 +525,7 @@ if ckpt_manager.latest_checkpoint:
     ckpt.restore(ckpt_manager.latest_checkpoint)
     print ('Latest checkpoint restored!!')
 
-EPOCHS = 1000
+EPOCHS = 200
 
 
 
