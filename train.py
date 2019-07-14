c03ba4500f482fe8bbfe1f0a03e539779a6ccee7
Andreas Iversen <Andreas-Hoie.Iversen@telenor.com>
2019-07-14 20:21:38 +0200

    dataset

--- train.py
+++ train.py
@@ -121,7 +121,11 @@ def tf_encode(pt, en):
 #    BATCH_SIZE, padded_shapes=([-1], [-1]))
 #train_dataset = train_dataset.prefetch(tf.data.experimental.AUTOTUNE)
 
-dataset = dataset.map(map_func=tf_encode, num_parallel_calls=4)
+dataset = dataset\
+    .map(map_func=tf_encode, num_parallel_calls=4)\
+    .filter(filter_max_length)\
+    .padded_batch(BATCH_SIZE, padded_shapes=([-1], [-1]))
+
 dataset = dataset.batch(batch_size=BATCH_SIZE)
 
 train_dataset = dataset
