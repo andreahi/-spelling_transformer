d79e43768b404230a636eef4e59acfc329baf26f
andreas <andreashi2@gmail.com>
2019-07-20 10:02:42 +0200

    dataset

--- train.py
+++ train.py
@@ -559,7 +559,7 @@ def train_step(inp, tar):
     train_accuracy(tar_real, predictions)
 
 def train_epoch(train_dataset, epoch):
-    for (inp, tar) in train_dataset.skip(32 * epoch).take(32):
+    for (inp, tar) in train_dataset.skip(epoch).take(32):
         #print(tar[0].numpy())
         train_step(inp, tar)
 
