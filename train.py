09cd5a83535c490f17a57ad512cdb1960514f9da
andreas <andreashi2@gmail.com>
2019-07-22 17:17:02 +0200

    dataset

--- train.py
+++ train.py
@@ -474,7 +474,7 @@ class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
 
 learning_rate = CustomSchedule(d_model)
 
-optimizer = tf.keras.optimizers.Adam(0.0001, beta_1=0.9, beta_2=0.98,
+optimizer = tf.keras.optimizers.Adam(0.00001, beta_1=0.9, beta_2=0.98,
                                      epsilon=1e-9)
 
 temp_learning_rate_schedule = CustomSchedule(d_model)
