57796015dbee593a8bbd814fe177cc3200012b87
andreas <andreashi2@gmail.com>
2019-07-28 18:53:35 +0200

    dataset

--- train.py
+++ train.py
@@ -124,7 +124,7 @@ def tf_encode(pt, en):
 train_dataset = dataset\
     .map(map_func=tf_encode, num_parallel_calls=4)\
     .filter(filter_max_length)\
-    .padded_batch(BATCH_SIZE, padded_shapes=([MAX_LENGTH], [15]))\
+    .padded_batch(BATCH_SIZE, padded_shapes=([MAX_LENGTH], [20]))\
     #.cache()
 train_dataset = train_dataset.prefetch(tf.data.experimental.AUTOTUNE)
 
