daa9bd401b352b6a0bb506a6706a07320522526e
andreas <andreashi2@gmail.com>
2019-08-07 20:30:12 +0200

    model size

--- train.py
+++ train.py
@@ -100,7 +100,7 @@ for ts in tokenized_string:
 
 
 BUFFER_SIZE = 20000
-BATCH_SIZE = 8
+BATCH_SIZE = 32
 
 
 def encode(lang1, lang2):
@@ -575,10 +575,10 @@ def train_step(inp, tar):
     train_accuracy(tar_real, predictions)
 
 def train_epoch(train_dataset, epoch):
-    for (inp, tar) in train_dataset.skip(0).take(1):
+    for (inp, tar) in train_dataset.skip(0).take(2):
         #print(inp[0].numpy())
         #print(tar[0].numpy())
-        train_step(inp, inp)
+        train_step(inp, tar)
 
 
 def train_model():
