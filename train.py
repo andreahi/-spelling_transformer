349b189512d1520d557bb716801555781aa07586
Andreas Iversen <Andreas-Hoie.Iversen@telenor.com>
2019-08-24 20:34:31 +0200

    validate

--- train.py
+++ train.py
@@ -491,7 +491,7 @@ class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
 
 learning_rate = CustomSchedule(d_model)
 
-optimizer = tf.keras.optimizers.Adam(0.0001)
+optimizer = tf.keras.optimizers.Adam(learning_rate)
 
 temp_learning_rate_schedule = CustomSchedule(d_model)
 
@@ -597,6 +597,7 @@ def train_epoch(train_dataset, epoch):
             train_loss.reset_states()
             train_accuracy.reset_states()
             start = time.time()
+            print("learning_rate: ", learning_rate)
 
 
 
